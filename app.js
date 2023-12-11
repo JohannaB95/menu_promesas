4be545e3df6723e29b88fcabc47bc674cf3ff2cf
JohannaB95 <johanna_b_g@hotmail.com>
2023-12-10 22:44:32 -0500

    Documentación del código

--- app.js
+++ app.js
@@ -1,27 +1,46 @@
-const colors = require('colors');
+//Se carga la librería colors para permitir el uso de estilos de colores en la terminal.
+require('colors');
 
+//Se carga el código que contiene el archivo 'menu' en la carpeta 'proy_modules'.
 const {mostrarMenu, pausa} = require('./proy_modules/menu');
 
+//Se declara una función flecha llamada main y que es asíncrona
 const main = async( ) => {
 
+    // Limpia la consola antes de imprimir el menú principal
     console.clear( );
 
+    //Se solicita imprimir un encabezado en la consola y que sea de color rojo, se añade diseño.
     console.log(`★★★★★★★★★★★★★★★★★★★★★★★★★★★`.blue);
     console.log(`★     `.blue +     `Menú principal`.red     +`      ★`.blue);
     console.log(`★★★★★★★★★★★★★★★★★★★★★★★★★★★\n`.blue);
 
+    //Se declara una variable llamada option y se inicializa con un string vacio
     let option = ' ';
 
+
+    //Este bucle se ejecutará al menos una vez y continuará repitiéndose mientras la variable option no sea igual a '0'
     do{
+        /*Este bucle do se ejecutará al menos una vez y continuará repitiéndose mientras la variable option no 
+        esté en el rango válido de opciones*/
         do{
+            /*Se utiliza la función mostrarMenu( ) para mostrar un menú al usuario y obtener la opción seleccionada.
+             El resultado se almacena en la variable option. */
             option = await mostrarMenu( );
         }while(! (option >=0 && option <=6))
 
+        /*Si option no es igual a '0' (salir), se imprime un mensaje indicando que se ejecutará el proceso 
+        asociado con la opción seleccionada.*/
         if (option !== '0'){
             console.log(`\n Se ejecuta el proceso ${option}`.green);
+            /*Se utiliza la función pausa( ) para pausar la ejecución del programa y esperar a que el usuario presione
+            una tecla antes de continuar*/
             await pausa( );
         }
+    /*El bucle se repetirá mientras option !== '0', lo que significa que continuará solicitando opciones del menú
+    hasta que el usuario elija salir. */
     }while(option !== '0');
 };
 
-main( );
+//Fin de la función main
+main();
--- proy_modules/menu.js
+++ proy_modules/menu.js
@@ -1,6 +1,9 @@
-const mostrarMenu = ( ) => {
+//Se crea una función constante que es una función flecha llamada mostrarMenu
+const mostrarMenu = () => {
 
-    return new Promise( resolve =>{
+     // Retorna una nueva promesa que se resuelve con el valor proporcionado en la función 'resolve'
+     return new Promise(resolve => {
+        // Muestra el menú en la consola con diferentes opciones, dependiendo de lo que se necesite realizar
         console.log(`★★★★★★★★★★★★★★★★★★★★★★★★★★★★★`.green);
         console.log(`★   `.green + `Seleccione una opción`.yellow +`   ★`.green);
         console.log(`★★★★★★★★★★★★★★★★★★★★★★★★★★★★★`.green);
@@ -13,33 +16,46 @@ const mostrarMenu = ( ) => {
         console.log(`★  `.green + `${'0.'}`.red  + `  Salir`.magenta+`                ★`.green);
         console.log(`★★★★★★★★★★★★★★★★★★★★★★★★★★★★★\n`.green);
 
+         // Crea una interfaz de lectura en la consola para permitir la entrada del usuario
         const readLine = require('readline').createInterface({
+            // Define el flujo de entrada, para leer la entrada del usuario desde la consola
             input: process.stdin,
+            //Define el flujo de salida, para mostrar mensajes en la consola
             output: process.stdout
         });
+        //Le indica al usuario que debe escoger una opción del menu
         readLine.question(`Selecione una opción: `.blue, (opt) => {
+            // Se cierra la interfaz de lectura
             readLine.close( );
+            // Se resuelve la promesa con la opción seleccionada por el usuario
             resolve(opt);
         })
     })
 };
 
-const pausa = ( ) => {
-    return new Promise (resolve =>{
+//Se crea una función constante que es una función flecha llamada pausa
+const pausa = () => {
 
+    // Retorna una nueva promesa que se resuelve con el valor proporcionado en la función 'resolve'
+    return new Promise(resolve => {
+        // Crea una interfaz de lectura en la consola para permitir la entrada del usuario
         const readLine = require('readline').createInterface({
+            // Define el flujo de entrada, para leer la entrada del usuario desde la consola
             input: process.stdin,
+            //Define el flujo de salida, para mostrar mensajes en la consola
             output: process.stdout
         })
-
-        readLine.question(`\n Presione ${'ENTER'} para continuar\n`.cyan, (opt) =>{
-            readLine.close( );
+        // Se le pide al usuario que presione ENTER para continuar
+        readLine.question(`\n Presione ${'ENTER'} para continuar\n`.cyan, (opt) => {
+            // Una vez que el usuario presiona ENTER, se cierra la interfaz de lectura y se resuelve la promesa
+            readLine.close();
             resolve(opt);
         })
     })
 };
 
+// Exporta un objeto que contiene varias funciones utilizadas en el módulo app.js
 module.exports = {
     mostrarMenu,
     pausa
-}
+};
